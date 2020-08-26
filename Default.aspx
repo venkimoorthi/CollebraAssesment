b0b73c04c8e5fe9555fb8182afebf23959e1d35e
venkimoorthi <70291514+venkimoorthi@users.noreply.github.com>
2020-08-27 01:37:07 +0530

    For Maersk
    Collebra Assessment for Maersk Bangalore

---
+++ Default.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table border="1">
+                <tr>
+                    <th colspan="4" style="text-align: center">Promotion Engine</th>
+                </tr>
+                <tr>
+                    <th>Item</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Option</th>
+                </tr>
+                <tr>
+                    <td>A : </td>
+                    <td>50 </td>
+                    <td>
+                        <asp:TextBox ID="txtQtyA" runat="server">0</asp:TextBox>
+                    </td>
+                    <td>
+                        <asp:CheckBox ID="chkA" runat="server" /></td>
+                </tr>
+                <tr>
+                    <td>B : </td>
+                    <td>30 </td>
+                    <td>
+                        <asp:TextBox ID="txtQtyB" runat="server">0</asp:TextBox>
+                    </td>
+                    <td>
+                        <asp:CheckBox ID="chkB" runat="server" /></td>
+                </tr>
+                <tr>
+                    <td>C : </td>
+                    <td>20 </td>
+                    <td>
+                        <asp:TextBox ID="txtQtyC" runat="server">0</asp:TextBox>
+                    </td>
+                    <td>
+                        <asp:CheckBox ID="chkC" runat="server" /></td>
+                </tr>
+                <tr>
+                    <td>D : </td>
+                    <td>15 </td>
+                    <td>
+                        <asp:TextBox ID="txtQtyD" runat="server">0</asp:TextBox>
+                    </td>
+                    <td>
+                        <asp:CheckBox ID="chkD" runat="server" />
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="4" style="text-align: center">
+                        <asp:Button ID="btnSubmit" runat="server" Text="Validate Order" OnClick="btnSubmit_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="4" style="text-align: center; color: green">
+                        <asp:Label ID="lblResult" runat="server" Text=""></asp:Label>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        lblResult.Text = "";
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        int qtyA = Convert.ToInt32(txtQtyA.Text.ToString());
+        int qtyB = Convert.ToInt32(txtQtyB.Text.ToString());
+        int qtyC = Convert.ToInt32(txtQtyC.Text.ToString());
+        int qtyD = Convert.ToInt32(txtQtyD.Text.ToString());
+
+        if (chkA.Checked && chkB.Checked && chkC.Checked && chkD.Checked)
+        {
+            if (qtyA == 1 && qtyB == 1 && qtyC == 1 && qtyD == 1)
+            {
+                callAPromotion(1, qtyA, qtyB, qtyC, qtyD);
+            }
+
+            if (qtyA >= 3 && qtyB == 1)
+            {
+                callAPromotion(2, qtyA, qtyB, qtyC, qtyD);
+            }
+
+            if (qtyB >= 2 && qtyA == 1)
+            {
+                callAPromotion(3, qtyA, qtyB, qtyC, qtyD);
+            }
+
+            if (qtyA >= 3 && qtyB >= 2) 
+            {
+                callAPromotion(4, qtyA, qtyB, qtyC, qtyD);
+            }
+
+            if (qtyA >= 3 && qtyB >= 2 && qtyC == 1 && qtyD == 1)
+            {
+                callAPromotion(5, qtyA, qtyB, qtyC, qtyD);
+            }
+
+            if (qtyA >= 3 && qtyB >= 2 && qtyC >= 1 && qtyD == 0)
+            {
+                callAPromotion(6, qtyA, qtyB, qtyC, qtyD);
+            }
+
+            if (qtyA >= 3 && qtyB >= 2 && qtyD >= 1 && qtyC == 0)
+            {
+                callAPromotion(7, qtyA, qtyB, qtyC, qtyD);
+            }
+
+            if (qtyA >= 3 && qtyB >= 2 && qtyC > 1 && qtyD == 1)
+            {
+                callAPromotion(8, qtyA, qtyB, qtyC, qtyD);
+            }
+
+            if (qtyA >= 3 && qtyB >= 2 && qtyD > 1 && qtyC == 1)
+            {
+                callAPromotion(9, qtyA, qtyB, qtyC, qtyD);
+            }
+        }
+
+        if (chkC.Checked && chkD.Checked && !chkA.Checked && !chkB.Checked)
+        {
+            if (qtyC == 1 && qtyD == 1)
+            {
+                callBPromotion(1, qtyA, qtyB, qtyC, qtyD);
+            }
+            else if (qtyC >= 1 && qtyD == 0)
+            {
+                callBPromotion(2, qtyA, qtyB, qtyC, qtyD);
+            }
+            else if (qtyD >= 1 && qtyC == 0)
+            {
+                callBPromotion(3, qtyA, qtyB, qtyC, qtyD);
+            }
+
+            if (qtyC > 1 && qtyD == 1)
+            {
+                callCPromotion(1, qtyA, qtyB, qtyC, qtyD);
+            }
+            else if (qtyD > 1 && qtyC == 1)
+            {
+                callCPromotion(2, qtyA, qtyB, qtyC, qtyD);
+            }
+        }
+    }
+
+    protected void callAPromotion(int option, int qtyA, int qtyB, int qtyC, int qtyD)
+    {
+        if (option == 1)
+        {
+            lblResult.Text = Convert.ToString(
+                                 qtyA * 50 +
+                                 qtyB * 30 +
+                                 qtyC * 20 +
+                                 qtyD * 15);
+        }
+        else if (option == 2)
+        {
+            lblResult.Text = Convert.ToString(
+                      (qtyA/3) * 130 + (qtyA%3) * 50 +
+                             qtyB * 30 +
+                             qtyC * 20 +
+                             qtyD * 15);
+        }
+        else if (option == 3)
+        {
+            lblResult.Text = Convert.ToString(
+                               qtyA * 50 +
+                               (qtyB/2) * 45 + (qtyB%2) * 30 +
+                                qtyC * 20 +
+                                qtyD * 15);
+        }
+        else if (option == 4)
+        {
+            lblResult.Text = Convert.ToString(
+                               (qtyA / 3) * 130 + (qtyA % 3) * 50 +
+                               (qtyB / 2) * 45 + (qtyB % 2) * 30 +
+                                qtyC * 20 +
+                                qtyD * 15);
+        }
+        else if (option == 5)
+        {
+            lblResult.Text = Convert.ToString(
+                             (qtyA / 3) * 130 + (qtyA % 3) * 50 +
+                             (qtyB / 2) * 45 + (qtyB % 2) * 30 +
+                             30);
+        }
+        else if (option == 6)
+        {
+            lblResult.Text = Convert.ToString(
+                             (qtyA / 3) * 130 + (qtyA % 3) * 50 +
+                             (qtyB / 2) * 45 + (qtyB % 2) * 30 +
+                             qtyC * 20);
+        }
+        else if (option == 7)
+        {
+            lblResult.Text = Convert.ToString(
+                             (qtyA / 3) * 130 + (qtyA % 3) * 50 +
+                             (qtyB / 2) * 45 + (qtyB % 2) * 30 +
+                             qtyD * 15);
+        }
+        else if (option == 8)
+        {
+            lblResult.Text = Convert.ToString(
+                             (qtyA / 3) * 130 + (qtyA % 3) * 50 +
+                             (qtyB / 2) * 45 + (qtyB % 2) * 30 +
+                              (((qtyC - qtyD) * 20) + (qtyD * 30)));
+        }
+        else if (option == 9)
+        {
+            lblResult.Text = Convert.ToString(
+                             (qtyA / 3) * 130 + (qtyA % 3) * 50 +
+                             (qtyB / 2) * 45 + (qtyB % 2) * 30 +
+                             (((qtyC - qtyD) * 15) + (qtyD * 30)));
+        }
+        else
+        {
+            lblResult.Text = "";
+        }
+    }
+
+    protected void callBPromotion(int option, int qtyA, int qtyB, int qtyC, int qtyD)
+    {
+        if (option == 1)
+        {
+            lblResult.Text = Convert.ToString(
+                             qtyA * 50 +
+                             qtyB * 30 +                              
+                             30);
+        }
+        else if (option == 2)
+        {
+            lblResult.Text = Convert.ToString(
+                             qtyA * 50 +
+                             qtyB * 30 +
+                             qtyC * 20);
+        }
+        else if (option == 3)
+        {
+            lblResult.Text = Convert.ToString(
+                             qtyA * 50 +
+                             qtyB * 30 +
+                             qtyD * 15);
+        }
+    }
+
+    protected void callCPromotion(int option, int qtyA, int qtyB, int qtyC, int qtyD)
+    {
+        if (option == 1)
+        {           
+            lblResult.Text = Convert.ToString(
+                             qtyA * 50 +
+                             qtyB * 30 +
+                             (((qtyC - qtyD) * 20) + (qtyD * 30)));
+        }
+        else if (option == 2)
+        {
+            lblResult.Text = Convert.ToString(
+                             qtyA * 50 +
+                             qtyB * 30 +
+                             (((qtyC - qtyD) * 15) + (qtyD * 30)));
+        }
+    }
+}
